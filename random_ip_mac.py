cef98ae6cfa0ca629ca6cd2ea1104c7168a1b327
王宇铖 <supermaxxx@sina.com>
2014-07-09 15:52:05 +0800

    Update random_ip_mac.py

--- random_ip_mac.py
+++ random_ip_mac.py
@@ -72,7 +72,7 @@ elif len_test < n:
     cha = len_test -n
     n_old = n
     n = len_test
-    print "Target is %s, but get %s. If you want target, please have another try as: python random_ip_mac.py %s\nresult is in the file %s(%s lines)." %(n_old,n,n_old,file,n)
+    print "Target is %s, but get %s. If you want target, please have another try.\nresult is in the file %s(%s lines)." %(n_old, n, file, n)
 
 msg = ''
 for i in range(n):
@@ -96,7 +96,7 @@ result is in the file 10.100_ip.sql(1000 lines).
 1000
 
 [root@10-9-12-126 wang]# python random_ip_mac.py 25000       
-Target is 25000, but get 24306. If you want target, please have another try as: python random_ip_mac.py 25000
+Target is 25000, but get 24306. If you want target, please have another try.
 result is in the file 10.100_ip.sql(24306 lines).
 [root@10-9-12-126 wang]# cat 10.100_ip.sql |wc -l
 24306
