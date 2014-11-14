3cda95371edbcf69ff9af9098ad601ca4543443d
王宇铖 <supermaxxx@sina.com>
2014-11-14 17:49:29 +0800

    Update magazine.py

--- magazine.py
+++ magazine.py
@@ -22,21 +22,13 @@ _vvshu_list_2 = []
 _madouer_list_1 = []
 _madouer_list_2 = []
 
-class Writer():
-    def __init__(self, filename):
-        self.filename = filename
-    def write(self, msg):
-        f = file(self.filename, 'a')
-        f.write(msg)
-        f.close()
-
 
 class Email(object):
     def __init__(self, MAIL_SUBJECT, MAIL_MESSAGE, ATTACHMENT=None):
-        self.MAIL_HOST = 'smtp.163.com'
-        self.MAIL_USERNAME = '******@163.com'
-        self.MAIL_PASSWORD = '******'
-        self.MAIL_TO = '******@qq.com'
+        self.MAIL_HOST = 'smtp.??.com'
+        self.MAIL_USERNAME = '??????@??.com'
+        self.MAIL_PASSWORD = '??????'
+        self.MAIL_TO = '??????@??.com'
         self.MAIL_SUBJECT = MAIL_SUBJECT  # title of the mail
         self.MAIL_MESSAGE = MAIL_MESSAGE  # body of the mail
         self.ATTACHMENT = ATTACHMENT  # attachment
@@ -66,69 +58,15 @@ class Email(object):
             logger.info(e)
 
 def add_head(filename):
-    stdout_pre = sys.stdout
-    sys.stdout = Writer(filename)
-    print '<html>'
-    print '<head>'
-    print '<meta http-equiv="Content-Language" content="zh-cn">'
-    print '<meta http-equiv="Content-Type" content="text/html; charset=gb2312">'
-    print '</head>'
-    print '<body onload="resizeimg();">'
-    print '<style> a {text-decoration: none;}</style>'
-    print
-    print '<script>'
-    print 'var count = 10;'
-    print
-    print 'function zoompic(obj)'
-    print '{'
-    print '  var s=obj.style.zoom;'
-    print '  if ((s== "undefined") || (s==""))'
-    print '  {'
-    print '     count=10;'
-    print '  }'
-    print '  else'
-    print '  {'
-    print '    l=s.length;'
-    print '    l=l-1;'
-    print '    s2=s.substr(0,l);'
-    print '    count=parseInt(s2);'
-    print '    count=count /10;'
-    print '    // alert(count);'
-    print '  }'
-    print '  if (count<2) count=2;'
-    print '  if (count>90) count=90;'
-    print '  if (event.wheelDelta >= 120)  count++;'
-    print '  else if (event.wheelDelta <= -120) count--;'
-    print "  obj.style.zoom = count + '0%';"
-    print '  return false;'
-    print '}'
-    print
-    print 'function Counting(count)'
-    print '{ '
-    print '  if (event.wheelDelta >= 120)  count++;'
-    print '  else if (event.wheelDelta <= -120) count--; '
-    print '  return count; '
-    print '}'
-    print '</script>'
-    print
-    print '<script>'
-    print 'function resizeimg()'
-    print '{'
-    print ' for (j=0;j<document.images.length;j++)'
-    print ' {'
-    print '  n= document.images[j].name;'
-    print '  if  (n.indexOf("p")==0)'
-    print ' }'
-    print '}'
-    print '</script>'
-    sys.stdout = stdout_pre
+    msg = '<html>\n<head>\n<meta http-equiv="Content-Language" content="zh-cn">\n<meta http-equiv="Content-Type" content="text/html; charset=gb2312">\n</head>\n<body onload="resizeimg();">\n<style> a {text-decoration: none;}</style>\n'
+    writelogfile(filename).log(msg)
 
 
 def  task_begin(name):
     _begin_time = datetime.now()
     begin_time = _begin_time.strftime("%Y-%m-%d %H:%M:%S")
     msg = "Downloading %s...BEGIN -- Current Time: %s" %(name, begin_time)
-    print msg
+    #print msg
     logger.info(msg)
     return _begin_time
 
@@ -244,8 +182,11 @@ def vvshu_run(LIST):
                     if q.split('/')[3] != day:
                         cmd = 'rm -rf %s' %q
                         os.system(cmd)
+                        msg = 'success to delete tired data: %s' %q
+                        print msg
+                        logger.info(msg)
             if work_dir != None:
-                htmfile = '%sview.htm' %work_dir
+                htmfile = '%s000.htm' %work_dir
                 cmd = 'rm -f %s' %htmfile
                 os.system(cmd)
                 add_head(htmfile)
@@ -281,11 +222,9 @@ def vvshu_run(LIST):
                             rc, out = commands.getstatusoutput(cmd)
                             if rc != 0:
                                 rc, out = commands.getstatusoutput(cmd)
-                    stdout_pre = sys.stdout
-                    sys.stdout = Writer(htmfile)
-                    print '<a href="%s" target=_blank><img border=0 src="%s"  onmousewheel="return zoompic(this);"></a><br><br>' %(_img, _img)
+                    msg = '<a href="%s" target=_blank><img border=0 src="%s"  onmousewheel="return zoompic(this);"></a><br><br>\n' %(_img, _img)
+                    writelogfile(htmfile).log(msg)
                     #print '<a target=_blank href="%s">%s</a>,978*1300<br>' %(_img, _img)
-                    sys.stdout = stdout_pre
                 cmd = 'ls %s | sed "s:^:%s/:"' %(work_dir[:-1], work_dir[:-1])
                 rc, out = commands.getstatusoutput(cmd)
                 if out != None:
@@ -295,7 +234,7 @@ def vvshu_run(LIST):
                 rc, out = commands.getstatusoutput(cmd)
                 if out != None:
                     sum = int(out.split()[0])
-                tar_count = (sum//(55*1024))+ 1
+                tar_count = (sum//(50*1024))+ 1
                 fb = fenbao(file_count, tar_count)
                 for n in range(0,len(fb)):
                     file_list_begin = fb[n][0]
@@ -304,20 +243,19 @@ def vvshu_run(LIST):
                     fb_file_list = ''
                     for o in files:
                         fb_file_list += ' ' + o
-                    _attr_path = '%s_%s.tar.gz' %(work_dir[:-1], str(n+1))
+                    _attr_path = '%s_%s.zip' %(work_dir[:-1], str(n+1))
                     attr_path = ''.join(chr(ord(x)) for x in _attr_path)
                     if len(fb) == 1:
                         title = text + ' all'
                     else:
-                        title = text + ' part ' + str(n+1) + ' of ' + str(len(fb))
+                        title = text + ' part ' + str(n+1) + ' of ' + str(len(fb)) + ' [Zip Begin at 2014-10-18]'
                     if os.path.exists(attr_path)  == False:
-                        cmd = 'cd %s && tar zcf %s %s' %(main_dir, attr_path, fb_file_list)
+                        cmd = 'zip -r %s %s' %(attr_path, fb_file_list)
                         rc, out = commands.getstatusoutput(cmd)
                         if rc == 0:
+                            print attr_path
+                            logger.info(attr_path)
                             Email(title, '', attr_path).run()
-                            cmd = "echo /dev/null > %s" %attr_path
-                            os.system(cmd)
-                    #Email(title, '', attr_path).run() #test
             task_end(_name, _begin_time)
 
 
@@ -338,8 +276,11 @@ def madouer_run(LIST):
                     if q.split('/')[3] != day:
                         cmd = 'rm -rf %s' %q
                         os.system(cmd)
+                        msg = 'success to delete tired data: %s' %q
+                        print msg
+                        logger.info(msg)
             if work_dir != None:
-                htmfile = '%sview.htm' %work_dir
+                htmfile = '%s000.htm' %work_dir
                 cmd = 'rm -f %s' %htmfile
                 os.system(cmd)
                 add_head(htmfile)
@@ -353,9 +294,9 @@ def madouer_run(LIST):
                     if (out[j-1], out[j]) == ('var', 'dir'):
                         dir = out[j+2][:-1].split("'")[1]
                 msg = '[' + str(i+1) + '] Pages: ' + str(page) + ' [' + text + ']'
+                img_url = base_img_url + dir
                 print msg
                 logger.info(msg)
-                img_url = base_img_url + dir
                 for m in range(1, page + 1):
                     _m = '%03d' %m
                     _img = _m + '.jpg'
@@ -369,11 +310,9 @@ def madouer_run(LIST):
                             rc, out = commands.getstatusoutput(cmd)
                             if rc != 0:
                                 rc, out = commands.getstatusoutput(cmd)
-                    stdout_pre = sys.stdout
-                    sys.stdout = Writer(htmfile)
-                    print '<a href="%s" target=_blank><img border=0 src="%s"  onmousewheel="return zoompic(this);"></a><br><br>' %(_img, _img)
+                    msg = '<a href="%s" target=_blank><img border=0 src="%s"  onmousewheel="return zoompic(this);"></a><br><br>\n' %(_img, _img)
+                    writelogfile(htmfile).log(msg)
                     #print '<a target=_blank href="%s">%s</a>,978*1300<br>' %(_img, _img)
-                    sys.stdout = stdout_pre
                 cmd = 'ls %s | sed "s:^:%s/:"' %(work_dir[:-1], work_dir[:-1])
                 rc, out = commands.getstatusoutput(cmd)
                 if out != None:
@@ -383,7 +322,7 @@ def madouer_run(LIST):
                 rc, out = commands.getstatusoutput(cmd)
                 if out != None:
                     sum = int(out.split()[0])
-                tar_count = (sum//(55*1024))+ 1
+                tar_count = (sum//(50*1024))+ 1
                 fb = fenbao(file_count, tar_count)
                 for n in range(0,len(fb)):
                     file_list_begin = fb[n][0]
@@ -392,20 +331,19 @@ def madouer_run(LIST):
                     fb_file_list = ''
                     for o in files:
                         fb_file_list += ' ' + o
-                    _attr_path = '%s_%s.tar.gz' %(work_dir[:-1], str(n+1))
+                    _attr_path = '%s_%s.zip' %(work_dir[:-1], str(n+1))
                     attr_path = ''.join(chr(ord(x)) for x in _attr_path)
                     if len(fb) == 1:
                         title = text + ' all'
                     else:
-                        title = text + ' part ' + str(n+1) + ' of ' + str(len(fb))
+                        title = text + ' part ' + str(n+1) + ' of ' + str(len(fb)) + ' [Zip Begin at 2014-10-18]'
                     if os.path.exists(attr_path)  == False:
-                        cmd = 'cd %s && tar zcf %s %s' %(main_dir, attr_path, fb_file_list)
+                        cmd = 'zip -r %s %s' %(attr_path, fb_file_list)
                         rc, out = commands.getstatusoutput(cmd)
                         if rc == 0:
+                            print attr_path
+                            logger.info(attr_path)
                             Email(title, '', attr_path).run()
-                            cmd = "echo /dev/null > %s" %attr_path
-                            os.system(cmd)
-                    #Email(title, '', attr_path).run() #test
             task_end(_name, _begin_time)
     
 
@@ -427,13 +365,6 @@ def fenbao(file_count, tar_count):
     return li
 
 
-def getcount(lis):
-    a = {}
-    for i in lis:
-        a[i] = lis.count(i)
-    return a
-
-
 def rebuild():
     for i in _vvshu_list_2:
         for j in i:
@@ -445,6 +376,7 @@ def rebuild():
                         else:
                             _madouers_1.append(j[0])
 
+
 class mylogger(object):
    def __init__(self,filename):
        self.filename = filename
@@ -454,8 +386,18 @@ class mylogger(object):
        return logger
 
 
+class writelogfile(object):
+    def __init__(self,logname):
+        self.logfilename = logname
+
+    def log(self,logmsg):
+        fd = open(self.logfilename, 'a')
+        fd.write(logmsg)
+        fd.close()
+
+
 if __name__ == '__main__':
-    main_dir = '/tmp/magazine'
+    main_dir = '/temp/magazine'  #main_dir, may be changed
     log_dir = main_dir + '/logs'
     if os.path.exists(log_dir) == False:
         os.system("mkdir -p %s" %log_dir)
@@ -463,6 +405,7 @@ if __name__ == '__main__':
     logger = mylogger("%s/magazine-%s.log" %(log_dir, today)).initlog()
     logger.info("Start!!!")
 
+    #index for madouer, need prevent
     index = { 'spring':'/snjt/spring/',
               'jelly':'/tmxg/jelly/',
               'more':'/yyol/more/',
@@ -475,22 +418,32 @@ if __name__ == '__main__':
               'mina':'/snjt/mina/',
               'nonno':'/snjt/nonno/'
     }
-
+    #name of magazine, need prevent, add/del
     _vvshus_1 =   ['minacn', 'raycn', 'ray', '25ans', 'gisele', 'ginger', 'steady', 'sweet', 'meirenbaihua', 'vivicn']
     _madouers_1 = ['spring', 'jelly', 'with', 'jj', 'faxing', 'biteki']
     repeat =      ['mina', 'nonno', 'vivi', 'cancam', 'more']
 
-    [vvshu(t).getlist(_vvshu_list_2) for t in repeat] #create _vvshu_list_2
-    [madouer(t).getlist(_madouer_list_2) for t in repeat] #create _madouer_list_2
+    #prepare
+    [vvshu(t).getlist(_vvshu_list_2) for t in repeat]  #create _vvshu_list_2, temp
+    [madouer(t).getlist(_madouer_list_2) for t in repeat]  #create _madouer_list_2, temp
     rebuild()
 
-    [vvshu(t).getlist(_vvshu_list_1) for t in _vvshus_1] #create _vvshu_list_1
-    [madouer(t).getlist(_madouer_list_1) for t in _madouers_1] #create _madouer_list_1
+    [vvshu(t).getlist(_vvshu_list_1) for t in _vvshus_1]  #create _vvshu_list_1, Finally
+    [madouer(t).getlist(_madouer_list_1) for t in _madouers_1]  #create _madouer_list_1, Finally
 
-    print "#############################"
-    print _vvshus_1
-    vvshu_run(LIST = _vvshu_list_1)
+    #run
+    msg = "######vvshu####################################################"
+    print msg
+    logger.info(msg)
+    msg = _vvshus_1
+    print msg
+    logger.info(msg)
+    vvshu_run(LIST = _vvshu_list_1)  #handle vvshu
     
-    print "#############################"
-    print _madouers_1
-    madouer_run(LIST = _madouer_list_1)
+    msg = "######madouer##################################################"
+    print msg
+    logger.info(msg)
+    msg = _madouers_1
+    print msg
+    logger.info(msg)
+    madouer_run(LIST = _madouer_list_1)  #handle madouer
