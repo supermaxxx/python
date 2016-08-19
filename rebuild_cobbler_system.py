016dca5dcfb324f9bd6bf7d70e6fc30ba3d6804b
王宇铖 <supermaxxx@sina.com>
2016-08-20 00:27:09 +0800

    Update rebuild_cobbler_system.py

--- rebuild_cobbler_system.py
+++ rebuild_cobbler_system.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import os
 import commands
 
@@ -26,14 +24,21 @@ def rebuild_system(pf, dic):
             os.system(cmd)   #add ipmilan
             print 'Done => %s' %cmd
         add_devs = v.get('add_devs')
-        if len(add_devs) > 0:
-            for i,j in add_devs.items():
-                if len(j) == 4:
-                    _add_gw = '--gateway %s' %j[3] if len(j[3])>0 else ''
-                elif len(j) == 3:
-                    _add_gw = ''
-                cmd = 'cobbler system edit --name=%s --interface=%s --mac=%s --ip-address=%s --subnet=%s %s --static=1' %(hostname, i, j[0], j[1], j[2], _add_gw)
-                os.system(cmd)   #add dev
+        if add_devs is not None:
+            if len(add_devs) > 0:
+                for i,j in add_devs.items():
+                    if len(j) == 4:
+                        _add_gw = '--gateway %s' %j[3] if len(j[3])>0 else ''
+                    elif len(j) == 3:
+                        _add_gw = ''
+                    cmd = 'cobbler system edit --name=%s --interface=%s --mac=%s --ip-address=%s --subnet=%s %s --static=1' %(hostname, i, j[0], j[1], j[2], _add_gw)
+                    os.system(cmd)   #add dev
+                    print 'Done => %s' %cmd
+        kickstart=v.get('kickstart')
+        if kickstart is not None:
+            if len(kickstart) > 0:
+                cmd = 'cobbler system edit --name=%s --kickstart=%s' %(hostname,kickstart)
+                os.system(cmd)   #add kickstart
                 print 'Done => %s' %cmd
         print '%s rebuild successfully.\n' %k
     print 'cobbler sync begin...'
@@ -43,31 +48,28 @@ def rebuild_system(pf, dic):
 
 
 ##data
-profile = "Centos7-cn0.ketong.com"
+profile = "CentOS7.2-x86_64"
 lis = {}
-lis['cns-1'] = {"hostname":"cns-1.10.179.2.1.phy.cns.cntest.keytone",
-                "ipmilan":("10.179.3.1", "root", "calvin"),
-                "add_devs":{
-                    "em1":('f0:4d:a2:08:46:e1', '10.179.2.1', '255.255.0.0', '10.179.0.254'),
-                }
+lis['B-control-1'] = {"hostname":"B-control-1",
+    "ipmilan":("10.100.0.201", "root", "calvin"),
+    "add_devs":{
+    "em4":('f1:2a:72:da:88:d3', '10.100.0.101', '255.255.255.0'),
+    },
+    "kickstart":"/var/lib/cobbler/kickstarts/B.ks",
 }
-lis['cns-2'] = {"hostname":"cns-2.10.179.2.2.phy.cns.cntest.keytone",
-                "ipmilan":("10.179.3.2", "root", "calvin"),
-                "add_devs":{
-                    "em1":('f0:22:19:61:e9:65', '10.179.2.2', '255.255.0.0', '10.179.0.254'),
-                }
+lis['B-cns-1'] = {"hostname":"B-cns-1",
+    "ipmilan":("10.100.0.204", "root", "calvin"),
+    "add_devs":{
+    "em4":('f1:ba:db:31:7f:39', '10.100.0.104', '255.255.255.0'),
+    },
+    "kickstart":"/var/lib/cobbler/kickstarts/B.ks",
 }
-lis['nat-1'] = {"hostname":"nat-1.10.179.6.1.phy.nat.cntest.keytone",
-                "ipmilan":("10.179.7.1", "root", "calvin"),
-                "add_devs":{
-                    "em1":('f0:22:19:63:31:62', '10.179.6.1', '255.255.0.0'),
-                }
-}
-lis['nat-2'] = {"hostname":"nat-2.10.179.6.2.phy.nat.cntest.keytone",
-                "ipmilan":("10.179.7.2", "root", "calvin"),
-                "add_devs":{
-                    "em1":('f0:22:19:62:34:15', '10.179.6.2', '255.255.0.0'),
-                }
+lis['B-nat-1'] = {"hostname":"B-nat-1",
+    "ipmilan":("10.100.0.207", "root", "calvin"),
+    "add_devs":{
+    "em4":('f1:4d:a2:08:78:c3', '10.100.0.107', '255.255.255.0'),
+    },
+    "kickstart":"/var/lib/cobbler/kickstarts/B.ks",
 }
 
 ##do
